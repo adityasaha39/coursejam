866d54a7019c4ad295fd414a513f982f7fcb0622
Aditya Saha <aditya.saha2017@gmail.com>
2022-05-24 18:37:55 +0530

    Backend Server Setup

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "React JS Tutorial",
+    image: "https://i.postimg.cc/YqdGQDhH/Capture.png",
+    description:
+      "Learn About React Class Components,Functional Component,Api Fetching,React Virtual DOM,Redux,React Routing and many more...",
+    brand: "Thapa Technical",
+    category: "Frontend",
+    price: 5.0,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Javascript ES6 Tutorial",
+    image: "https://i.postimg.cc/zBD3Ln8B/Capture.png",
+    description:
+      "Introducing to the Javascript ES6. Tutorial on Async/Await,Fetch Api,DOM Manupulation,Local Storage,Arrow Function and many more...",
+    brand: "Code With Harry",
+    category: "Language",
+    price: 3.5,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "C++ Tutorial",
+    image: "https://i.postimg.cc/hjwJHXRt/Capture.png",
+    description:
+      "Learn Data Structure and algorithm with C++,Linked List,Stack,Queue,Tree,Graph,Binary Search,Sorting Algorithm and many more...",
+    brand: "Apni Khaksa",
+    category: "Language",
+    price: 2.0,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Node Js And Express Js",
+    image: "https://i.postimg.cc/gjHCxBqX/Capture.png",
+    description:
+      "Introduction to Backend using Node JS and Express Js,learn about backend server,how to create api,depolyment of backend using heroku and many more",
+    brand: "Thapa Technical",
+    category: "Backend",
+    price: 4.5,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "App Devolopement with Kotlin",
+    image: "https://i.postimg.cc/02g3vxvt/Capture.png",
+    description:
+      "Real-world Android Projects for Beginners to Master Android Development. Recommended for learning Android development from scratch",
+    brand: "Anuj Bhaiya",
+    category: "Android",
+    price: 5.5,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "Mongo DB and Mongoose",
+    image: "https://i.postimg.cc/d1PQdpd4/Capture.png",
+    description:
+      "Introduction to Mongo DB,Learn crud operation in Mongo DB,using of mongoose in Node Js,Mongo DB Atlas and many more...",
+    brand: "Thapa Technical",
+    category: "Database",
+    price: 2.5,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+module.exports = products;
