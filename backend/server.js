010c711011c00405f23a0b89f0c0663d51b11d2e
Aditya Saha <aditya.saha2017@gmail.com>
2022-06-25 00:40:19 +0530

    Implemented Middleware

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config.js/db.js";
-import products from "./data/products.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -18,16 +19,10 @@ app.get("/", (req, res) => {
   res.send("Backend Server of COURSEJAM is Running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => {
-    return p._id === req.params.id;
-  });
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
